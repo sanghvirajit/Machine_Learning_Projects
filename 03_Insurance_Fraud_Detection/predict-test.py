b84962269ace42597fa5b4b301bcc570f845021e
sanghvirajit19 <sanghvirajit19@gmail.com>
2021-10-26 20:45:30 +0200

    Insurance_fraud_detection

---
+++ 03_Insurance_Fraud_Detection/predict-test.py
@@ -0,0 +1,24 @@
+import requests
+import pandas as pd
+import json
+
+url = 'http://localhost:9696/predict'
+
+#host = 'churn-serving-env.eba-gyzkzxig.eu-central-1.elasticbeanstalk.com'
+#url = f'http://{host}/predict_flask'
+
+df = pd.read_csv("customer_test.csv")
+
+customer_number = 23
+
+customer = df.iloc[customer_number-1].to_json()
+customer = json.loads(customer)
+
+response = requests.post(url, json=customer).json()
+print(response)
+
+if response['Fraud'] == True:
+    print("It's a fraud claim")
+else:
+    print("Claim is legit")
+
